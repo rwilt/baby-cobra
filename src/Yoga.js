27fc58ae16a7f07f0b553c9e13681a5a9fba4af5
rwilt <rosie.wilt@gmail.com>
2020-09-17 21:22:10 -0400

    yoga page

---
+++ src/Yoga.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Yoga(props) {
+    return (
+        <div>
+        <div className="yoga-div">
+        <h1>Private Yoga</h1>
+        <p>$40</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        </div>
+        <div className="group-yoga">
+        <h1>Group Yoga</h1>
+        <p>$20pp</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+        </div>
+        </div>
+    )
+}
+
+export default Yoga;
